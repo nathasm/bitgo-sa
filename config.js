cb222a40000f97e3820d647ddec61e66f5958acc
Nathan Smith <nathan.smith@thesystech.com>
2019-03-20 21:12:47 -0700

    Application configuration information

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  update_interval: 500,
+  stream_path: 'statuses/filter',
+  keywords: [
+    'twitter',
+    'facebook',
+    'google',
+    'travel',
+    'art',
+    'music',
+    'photography',
+    'love',
+    'fashion',
+    'food'
+  ]
+};
