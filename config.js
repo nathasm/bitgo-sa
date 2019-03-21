3f2b880df125de8ce049c500c75e1ac4aabe0f78
Nathan Smith <nathan.smith@thesystech.com>
2019-03-20 21:43:46 -0700

    Use env variables for tokens

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   update_interval: 500,
   stream_path: 'statuses/filter',
   keywords: [
